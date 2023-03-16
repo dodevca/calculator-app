681626cb8337bd77974f5069353dfdc833591cad
twelveio <twelvedotio@gmail.com>
2023-03-16 14:48:27 +0700

    edit function main

--- Calculator.cs
+++ Calculator.cs
@@ -2,15 +2,21 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10, b = 6;
 		
+		Console.writeLine(""Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b), Pengurangan(a, b));
+		Console.writeLine(""Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b), Pengurangan(a, b));
+	
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
-	static int penambahan(int a, int b)
+	static int Penambahan(int a, int b)
 	{
 		return a + b;
 	}
 	
-	static int pengurangan(int a, int b)
+	static int Pengurangan(int a, int b)
 	{
 		return a - b;
 	}
